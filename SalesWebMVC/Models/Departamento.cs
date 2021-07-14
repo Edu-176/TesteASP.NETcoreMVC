2dcffffc17aa6ce837d24faa325e8487f69d5985
Edu-176 <eduardomatanob@gmail.com>
2021-07-14 14:20:46 -0300

    Criacao do departamento

---
+++ SalesWebMVC/Models/Departamento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Departamento
+    {
+        private int _id;
+        private string _name;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}
